029acad6f71040c12cd30d577a96edc6a8ffd779
susil <thapasusil53@gmail.com>
2020-07-19 12:11:46 +0545

    first commit- all rows are fetched

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Row from './Row'
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NepaFlix</h1>
+      
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trendig Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comidy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horro Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,17 @@
+const API_KEY = '49b448bd801f342d60a187eeb5939faf'
+
+const END_POINT = 'https://api.themoviedb.org/3/movie/550?api_key=49b448bd801f342d60a187eeb5939faf'
+
+
+const requests ={
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
